7ed16cb2ffc6c75abb3c0bc03d9c02fdd8e6026f
Alina-Pishak <alinapishak@icloud.com>
2023-09-23 12:43:55 +0300

    add pagination

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,45 +1,67 @@
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
-import React, { useState, useEffect } from 'react';
-import { async } from 'q';
+import React, { useState, useEffect, useCallback } from 'react';
 
 export const Gallery = () => {
   const [searchValue, setSearchValue] = useState('');
   const [page, setPage] = useState(1);
   const [photo, setPhoto] = useState([]);
+  const [showBtn, setShowBtn] = useState(false);
 
-  useEffect(() => {
-    if (!searchValue) return;
+  const getNormalizedPhotos = arr => {
+    return arr.map(({ id, alt, src, avg_color }) => ({
+      id,
+      alt,
+      src,
+      avg_color,
+    }));
+  };
 
-    getImages();
-  }, [searchValue, page]);
-
-  const getImages = async () => {
+  const getImages = useCallback(async () => {
     try {
       const { photos, total_results } = await ImageService.getImages({
         query: searchValue,
         page,
       });
-      setPhoto(photos);
+      setPhoto(prev => [...prev, ...getNormalizedPhotos(photos)]);
+      setShowBtn(page < Math.ceil(total_results / 15));
     } catch ({ message }) {
       console.log(message);
     }
-  };
+  }, [searchValue, page]);
+
+  useEffect(() => {
+    if (!searchValue) return;
+
+    getImages();
+  }, [searchValue, getImages]);
 
   const onSubmit = value => {
+    setPhoto([]);
+    setPage(1);
     setSearchValue(value);
+  };
+
+  const handlerClick = () => {
+    setPage(prev => prev + 1);
   };
 
   return (
     <>
       <SearchForm onSubmit={onSubmit}></SearchForm>
-      <Grid>
-        <GridItem>
-          <CardItem></CardItem>
-        </GridItem>
-      </Grid>
-      <Button></Button>
+      {photo.length > 0 && (
+        <Grid>
+          {photo.map(({ id, alt, src, avg_color }) => (
+            <GridItem key={id}>
+              <CardItem color={avg_color}>
+                <img src={src.small} alt={alt} />
+              </CardItem>
+            </GridItem>
+          ))}
+        </Grid>
+      )}
+      {showBtn && <Button onClick={handlerClick}>Load more</Button>}
     </>
   );
 };
