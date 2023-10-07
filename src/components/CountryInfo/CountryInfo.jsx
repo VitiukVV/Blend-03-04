896b05a9f0ea90b70200ce58834c00a40dbaec53
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-10-07 11:34:57 +0200

    task 123

--- src/components/CountryInfo/CountryInfo.jsx
+++ src/components/CountryInfo/CountryInfo.jsx
@@ -10,34 +10,35 @@ import {
 } from './CountryInfo.styled';
 
 export const CountryInfo = ({
-  flag,
-  capital,
-  country,
-  id,
-  languages = [],
-  population,
+  country: {
+    flag,
+    capital,
+    countryName,
+
+    languages = [],
+    population,
+  },
 }) => {
   return (
-    <h2>CountryInfo</h2>
-    // <CountryWrapper >
-    //   <Flag>
-    //     <Image />
-    //   </Flag>
-    //   <CountryDescription>
-    //     <CountryCapital>
-    //       Capital: <Accent></Accent>
-    //     </CountryCapital>
+    <CountryWrapper>
+      <Flag>
+        <Image src={flag} alt={countryName} />
+      </Flag>
+      <CountryDescription>
+        <CountryCapital>
+          Capital: <Accent>{capital.join(', ')}</Accent>
+        </CountryCapital>
 
-    //     <CountryTitle></CountryTitle>
+        <CountryTitle>{countryName}</CountryTitle>
 
-    //     <CountryDetail>
-    //       Population: <Accent></Accent>
-    //     </CountryDetail>
+        <CountryDetail>
+          Population: <Accent>{population}</Accent>
+        </CountryDetail>
 
-    //     <CountryDetail>
-    //       Languages: <Accent></Accent>
-    //     </CountryDetail>
-    //   </CountryDescription>
-    // </CountryWrapper>
+        <CountryDetail>
+          Languages: <Accent>{languages.join(', ')}</Accent>
+        </CountryDetail>
+      </CountryDescription>
+    </CountryWrapper>
   );
 };
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,34 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useCallback, useState, useEffect } from 'react';
+import { fetchCountry } from 'service/country-service';
+import { useParams } from 'react-router-dom';
 
 export const Country = () => {
+  const [country, setCounrty] = useState(null);
+  const [isLoader, setIsLoader] = useState(false);
+  const { id } = useParams();
+  const fetchCountryId = useCallback(async () => {
+    setIsLoader(true);
+    try {
+      const data = await fetchCountry(id);
+      console.log(data);
+      setCounrty(data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoader(false);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (!id) return;
+    fetchCountryId();
+  }, [fetchCountryId, id]);
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        {isLoader && <Loader />}
+        {country && <CountryInfo country={country} />}
       </Container>
     </Section>
   );
