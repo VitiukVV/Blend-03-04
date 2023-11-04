9106828f879d74140cfc20dded41d17556bb21ab
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-11-04 09:53:10 +0100

    first commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,75 +1,20 @@
-import { nanoid } from "nanoid";
-
-import {
-  Container,
-  Grid,
-  GridItem,
-  Header,
-  SearchForm,
-  Section,
-  Text,
-  Todo,
-} from "components";
-import { useEffect, useState } from "react";
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Layout } from '../../layout/Layout/Layout';
+import { routes } from '../../routes';
+import { Home, Options } from '../../views';
 
 export const App = () => {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    const todos = JSON.parse(localStorage.getItem("todos"));
-
-    if (todos) {
-      setTodos(todos);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
-  const addTodo = (text) => {
-    const todo = {
-      id: nanoid(),
-      text,
-    };
-
-    setTodos((todos) => [...todos, todo]);
-  };
-
-  const handleSubmit = (data) => {
-    addTodo(data);
-  };
-
-  const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((todo) => todo.id !== id));
-  };
-
   return (
     <>
-      <Header />
-      <Section>
-        <Container>
-          <SearchForm onSubmit={handleSubmit} />
+      <Routes>
+        <Route path={routes.HOME} element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path={routes.OPTIONS} element={<Options />} />
+        </Route>
 
-          {todos.length === 0 && (
-            <Text textAlign="center">There are no any todos ... </Text>
-          )}
-
-          <Grid>
-            {todos.length > 0 &&
-              todos.map((todo, index) => (
-                <GridItem key={todo.id}>
-                  <Todo
-                    id={todo.id}
-                    text={todo.text}
-                    counter={index + 1}
-                    onClick={deleteTodo}
-                  />
-                </GridItem>
-              ))}
-          </Grid>
-        </Container>
-      </Section>
+        <Route path='*' element={<Navigate to={routes.HOME} replace />} />
+      </Routes>
     </>
   );
 };
